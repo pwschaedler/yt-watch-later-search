5ec7816f115e382cfc1dce6645daf8aadb70a50a
Peter Schaedler <pwschaedler@gmail.com>
2024-05-14 23:00:19 -0700

    Read multiple pages and print a bit more info

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module yt-watch-later-search
 go 1.21.3
 
 require (
-	golang.org/x/net v0.17.0
 	golang.org/x/oauth2 v0.13.0
 	google.golang.org/api v0.147.0
 )
@@ -19,6 +18,7 @@ require (
 	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
--- yt-watch-later-search.go
+++ yt-watch-later-search.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"os/user"
 	"path/filepath"
+	"time"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -123,18 +124,63 @@ func main() {
 
 	// Get videos from playlist
 	// PLMsroFQMqFwI082s1_mf4oU9ILn-09PfM
+	fmt.Println("In case you forgot: PLMsroFQMqFwI082s1_mf4oU9ILn-09PfM")
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Print("Playlist ID: ")
 	scanner.Scan()
 	playlistId := scanner.Text()
 	// fmt.Println(playlistId)
 
+	// Getting PublishedAt date doesn't work, that gives the date the video was
+	// put into the playlist. Would have to request videos themselves to get
+	// actual video date. Probably pull list of video IDs, do a big batch
+	// request to get video info for all, and then pull info from there.
+	// Or use "contentDetails" instead of "snippet" and get videoPublishedAt
+	// from there.
+
+	totalResults := 0
+
 	videoCall := youtubeService.PlaylistItems.List([]string{"snippet"})
 	videoCall = videoCall.MaxResults(50)
 	videoCall = videoCall.PlaylistId(playlistId)
 	resp, err := videoCall.Do()
 	handleError(err, "Couldn't get videos from playlist.")
+	// var t time.Time
 	for _, item := range resp.Items {
-		fmt.Println(item.Snippet.Title)
+		// t, _ = time.Parse(time.RFC3339, item.Snippet.PublishedAt)
+		// , t.Format(time.DateOnly)
+		// fmt.Printf("Date compare -- %v // %v\n", item.Snippet.PublishedAt, t)
+		fmt.Printf("[%v]  %v\n", item.Snippet.VideoOwnerChannelTitle, item.Snippet.Title)
 	}
+
+	totalResults += len(resp.Items)
+	fmt.Println()
+	fmt.Printf("Read %v items total\n", totalResults)
+
+	nextPageToken := resp.NextPageToken
+	fmt.Printf("nextPageToken: %v\n", nextPageToken)
+	fmt.Printf("nextPageToken is null: %v\n", nextPageToken == "")
+	fmt.Println()
+
+	for nextPageToken != "" {
+		fmt.Println("Sleeping 5 sec ...")
+		time.Sleep(5 * time.Second)
+
+		videoCall.PageToken(nextPageToken)
+		resp, err = videoCall.Do()
+		handleError(err, "Couldn't get videos from playlist.")
+		for _, item := range resp.Items {
+			// t, _ = time.Parse(time.RFC3339, item.Snippet.PublishedAt)
+			fmt.Printf("[%v]  %v\n", item.Snippet.VideoOwnerChannelTitle, item.Snippet.Title)
+		}
+		totalResults += len(resp.Items)
+		fmt.Println()
+		fmt.Printf("Read %v items total\n", totalResults)
+		nextPageToken = resp.NextPageToken
+		fmt.Printf("nextPageToken: %v\n", nextPageToken)
+		fmt.Printf("nextPageToken is null: %v\n", nextPageToken == "")
+		fmt.Println()
+	}
+
+	fmt.Println("Done!")
 }
